3dd9809af022a581fb587519ee64087006b1bb56
Emanuele Viglianisi <emanuele.viglianisi@runtastic.com>
2020-10-06 17:26:30 +0200

    demo changes 5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='analyticsbackend',
     packages=find_packages(),
-    version='0.1.4',
+    version='0.1.5',
     description='Databricks Labs CICD Templates Sample Project',
     author='Emanuele Viglianisi',
     license='',
